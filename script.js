725fb520d08aa2189923f925364013fb65827f65
JLC <jlc@annonymous.com>
2020-08-02 10:11:38 -0500

    Styling time-blocks container

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
     ////////--->Variables declarations<---////////
-    const currentdate = $("#currentDay"); // <p> element that renders the current day
+    const currentday = $("#currentDay"); // <p> element that renders the current day
     const container = $(".container"); // <div> elemnt that appends to render the timeblocks
-    let curdate = moment().format('MMMM Do YYYY'); //Variable to get the current date from moment.js library
+    let curday = moment().format('MMMM Do YYYY'); //Variable to get the current date from moment.js library
     ////////--->End of Variables declarations<---////////
 
-    //////Render current date
-    function renderCurrentDate() {
-        currentdate.text(curdate);
+    //////Render current day
+    function renderCurrentDay() {
+        currentday.text(curday);
     }
-    renderCurrentDate();
+    renderCurrentDay();
+
+
+    //////Render time blocks
+    function renderTimeBlocks() {
+
+    }
 });
